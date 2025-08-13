42e201b89d0829293ba5002e9bad8dcc5e4deabf
amitpal603-ops <ap036776@gmail.com>
2025-08-13 13:42:57 +0530

    update expense

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import ExpenseTable from './components/ExpenseTable'
 
 function App() {
   return (
-    <div className='flex  flex-col justify-center  mt-8 gap-30 items-center md:flex' >
-      <ExpenseForm></ExpenseForm>
-      <ExpenseTable/>
+    <div className="flex flex-col items-center gap-8 px-4 mt-8 lg:flex-row lg:items-start lg:justify-center">
+      {/* Form */}
+      <div className="w-full max-w-md">
+        <ExpenseForm />
+      </div>
+
+      {/* Table */}
+      <div className="w-full max-w-3xl">
+        <ExpenseTable />
+      </div>
     </div>
   )
 }
